4477c0b7ed0c324aeb48eb0b700232ae08399a59
DFVasco <danielvasco.0112@gmail.com>
2024-08-26 19:10:07 -0500

    First user story completed HU1

--- src/main/java/com/emazon/stockservice/infrastructure/input/rest/CategoriaRestController.java
+++ src/main/java/com/emazon/stockservice/infrastructure/input/rest/CategoriaRestController.java
@@ -3,37 +3,48 @@ package com.emazon.stockservice.infrastructure.input.rest;
 import com.emazon.stockservice.application.dto.CategoriaDTORequest;
 import com.emazon.stockservice.application.dto.CategoriaDTOResponse;
 import com.emazon.stockservice.application.handler.iCategoriaHandler;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/categoria")
+@Tag(name = "Categorías", description = "Operaciones relacionadas con las categorías")
 @RequiredArgsConstructor
 public class CategoriaRestController {
     private final iCategoriaHandler categoriaHandler;
 
     @PostMapping("/add")
-    public ResponseEntity<Void> createCategoriaInStockService(@RequestBody CategoriaDTORequest categoriaDTORequest) {
+    @Operation(summary = "Crear una nueva categoría")
+    @ApiResponse(responseCode = "201", description = "Categoría creada con éxito")
+    public ResponseEntity<Void> createCategoriaInStockService(@Valid @RequestBody CategoriaDTORequest categoriaDTORequest) {
         categoriaHandler.createCategoriaInStockService(categoriaDTORequest);
         return ResponseEntity.status(201).build();
     }
     @GetMapping("/getAll")
+    @Operation(summary = "Obtener todas las categorías")
     public ResponseEntity<List<CategoriaDTOResponse>> getAllCategoriasFromStockService(){
         return ResponseEntity.ok(categoriaHandler.getAllCategoriasFromStockService());
     }
     @GetMapping("/getBy/{id}")
+    @Operation(summary = "Obtener una categoria por su id")
     public ResponseEntity<CategoriaDTOResponse> getCategoriaFromStockService(@PathVariable(name="id") Long id){
         return ResponseEntity.ok(categoriaHandler.getCategoriaFromStockService(id));
     }
     @PutMapping("/update")
+    @Operation(summary = "Actualiza la categoria seleccionada")
     public ResponseEntity<Void> updateCategoriaFromStockService(@RequestBody CategoriaDTORequest categoriaDTORequest){
         categoriaHandler.updateCategoriaInStckService(categoriaDTORequest);
         return ResponseEntity.noContent().build();
     }
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Elimina la categoria por su id")
     public ResponseEntity<Void> deleteCategoriaFromStockService(@PathVariable(name="id") Long id){
         categoriaHandler.deleteCategoriaFromStockService(id);
         return ResponseEntity.noContent().build();
---
+++ src/test/java/com/emazon/stockservice/CategoriaEntityMapperTest.java
@@ -0,0 +1,68 @@
+package com.emazon.stockservice;
+
+import com.emazon.stockservice.domain.model.Categoria;
+import com.emazon.stockservice.infrastructure.output.jpa.entity.CategoriaEntity;
+import com.emazon.stockservice.infrastructure.output.jpa.mapper.CategoriaEntityMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class CategoriaEntityMapperTest {
+
+    @Autowired
+    private CategoriaEntityMapper mapper;
+
+    @Test
+    void testToCategoriaEntity() {
+        Categoria categoria = new Categoria(1L, "Electrónica", "Productos tecnológicos");
+
+        CategoriaEntity categoriaEntity = mapper.toCategoriaEntity(categoria);
+
+        assertNotNull(categoriaEntity);
+        assertEquals(categoria.getId(), categoriaEntity.getId());
+        assertEquals(categoria.getNombre(), categoriaEntity.getNombre());
+        assertEquals(categoria.getDescripcion(), categoriaEntity.getDescripcion());
+    }
+
+    @Test
+    void testToCategoria() {
+        CategoriaEntity categoriaEntity = new CategoriaEntity(1L, "Electrónica", "Productos tecnológicos");
+
+        Categoria categoria = mapper.toCategoria(categoriaEntity);
+
+        assertNotNull(categoria);
+        assertEquals(categoriaEntity.getId(), categoria.getId());
+        assertEquals(categoriaEntity.getNombre(), categoria.getNombre());
+        assertEquals(categoriaEntity.getDescripcion(), categoria.getDescripcion());
+    }
+
+    @Test
+    void testToCategoriaList() {
+        CategoriaEntity categoriaEntity1 = new CategoriaEntity(1L, "Electrónica", "Productos tecnológicos");
+        CategoriaEntity categoriaEntity2 = new CategoriaEntity(2L, "Ropa", "Ropa y accesorios");
+        List<CategoriaEntity> categoriaEntityList = Arrays.asList(categoriaEntity1, categoriaEntity2);
+
+        List<Categoria> categoriaList = mapper.toCategoriaList(categoriaEntityList);
+
+        assertNotNull(categoriaList);
+        assertEquals(2, categoriaList.size());
+
+        Categoria categoria1 = categoriaList.get(0);
+        Categoria categoria2 = categoriaList.get(1);
+
+        assertEquals(categoriaEntity1.getId(), categoria1.getId());
+        assertEquals(categoriaEntity1.getNombre(), categoria1.getNombre());
+        assertEquals(categoriaEntity1.getDescripcion(), categoria1.getDescripcion());
+
+        assertEquals(categoriaEntity2.getId(), categoria2.getId());
+        assertEquals(categoriaEntity2.getNombre(), categoria2.getNombre());
+        assertEquals(categoriaEntity2.getDescripcion(), categoria2.getDescripcion());
+    }
+}
